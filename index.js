b5f91d2663d69f66384741654a50e1f306338a6d
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2024-02-15 15:00:47 -0500

    updated js file

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 
-var form = document.querySelector('form')
+var ul = document.getElementById('ul')
+var form = document.getElementById('add-todo');
+var  toDolist = document.getElementById('todo-list');
+var input = document.querySelector(input);
 
 
+function resetInput() {
+    toDoItem = "";
+}
+
+//call form / access form data /create input
 form.onsubmit = function(e) {
     //don't forget to prevent the form submission
     e.preventDefault()
-    console.log('submitted')
+    console.log('submitted');//show submitted
     //access form data by targeting the input
-    var listItem = document.querySelector('input')
-    console.log(listItem.value)
+    var toDoItem = document.querySelector('input');
+    console.log(toDoItem.value);
+    
+    // check to see if toDoItem is on the list already, if not continue
+    if (toDoItem !== "") {
+        var toDoList = document.getElementById('#toDoList');
+        var button = document.createElement('button');//create button El
+        var myList = document.createElement('li');//create li El
+        button.textContent = toDoItem;
+        console.log(toDoItem,'readdy to log item')
+        
+        toDoItem.appendChild(myList)//this is the li list
+        myList.appendChild(toDoItem)
+        
+        //myList.document.querySelector('#todo-list').textContent = toDoItem
+
+        console.log(myList.value, 'toDoItem');
+        
+         //reset value
+        toDoItem = " ";
+        
+    }
     //reset the form fields after console.logging the item
-    listItem.value = ''
+    toDoItem.value = "";
+    console.log(toDoItem, "return to empty");
+   
+    //add items to the list 
     
 }
-
-//add list (li) to (ul)
-const ul = document.createElement('ul')
-document.body.appendChild(ul);
-
-const li1 = document.createElement('li');
-const li2 = document.createElement('li');
-const li3 = document.createElement('li');
-const li4 = document.createElement('li');
-console.log(li1,li2,li3,li4)
-
-ul.appendChild(li1)
-ul.appendChild(li2)
-ul.appendChild(li3)
-ul.appendChild(li4)
-
 
 
 
